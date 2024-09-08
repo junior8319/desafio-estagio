a108c502f5af3f4a720b811bb9055719b84a6ea5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-09-08 15:22:58 -0300

    correções-Exercício 2-melhoria da assertividade da verificação (indo até o próximo maior que n na sequência) e retorno mensagem correspondente da posição

--- fibonacci.js
+++ fibonacci.js
@@ -1,47 +1,58 @@
 const prompt = require('prompt-sync');
 const readline = prompt();
 
-// 2) Calcular fibonacci até um índice n
-const fibonacci = (n, primeiro = 0, segundo = 1) => {
-  let listaFibonacciResultante = [primeiro, segundo];
-  let anterior = segundo;
-  let proximo = segundo + primeiro;
+// Função que retorna a mensagem de acordo com a presença do número na sequência de Fibonacci
+const retornarMensagem = (n, listaFibonacciResultante) => {
   const posicao = listaFibonacciResultante.findIndex((elemento) => elemento === n);
-
-  if (n === primeiro) {
-    return `O numero informado ${n} está na sequência de Fibonacci, com o índice:
-    ${posicao}, ou seja, na posição ${posicao + 1} da sequência, em ${listaFibonacciResultante}.`;
-  }
-
-  if (n === segundo) {
-    return `O numero informado ${n} está na sequência de Fibonacci, com o índice:
-    ${posicao}, ou seja, na posição ${posicao + 1} da sequência, em ${listaFibonacciResultante}.`;
-  }
-
-  for (let indice = 2; indice < n; indice += 1) {
-    listaFibonacciResultante.push(proximo);
-    anterior = proximo;
-    proximo = proximo + listaFibonacciResultante[indice - 1];
-  }
-
-
+  
+  const mensagemPositiva = `O numero informado ${n} está na sequência de Fibonacci, com o índice:
+    ${posicao}, ou seja, na ${posicao + 1}ª posição da sequência, em ${listaFibonacciResultante}.\n`;
+  
+  const mensagemNegativa = `O número informado ${n} não está na sequência de Fibonacci,
+    mas a sequência que inclui o maior mais próximo é:
+    ${listaFibonacciResultante}.\n`;
+  
   if (posicao !== -1) {
-    return `O numero informado ${n} está na sequência de Fibonacci, com o índice:
-      ${posicao}, ou seja, na posição ${posicao + 1} da sequência, em ${listaFibonacciResultante}.`;
+    return mensagemPositiva;
   }
-
-  return `O número informado ${n} não está na sequência de Fibonacci até a posição ${n},
-    mas a sequência com ${n} números é:
-    ${listaFibonacciResultante}.`;
+  
+  return mensagemNegativa;
 }
 
-let n = 0;
+// Calcular fibonacci até um índice n
+const fibonacci = (n, primeiro = 0, segundo = 1) => {
+  let listaFibonacciResultante = [primeiro, segundo];
+  let indiceUltimo = listaFibonacciResultante.length - 1;
+  let ultimo = listaFibonacciResultante[listaFibonacciResultante.length - 1];
+  let proximo = segundo + primeiro;
+  
+  if (n === primeiro) {
+    return retornarMensagem(0, listaFibonacciResultante);
+  }
+  
+  if (n === segundo) {
+    return retornarMensagem(1, listaFibonacciResultante);
+  }
+
+  while (ultimo <= n) {    
+    proximo = ultimo + listaFibonacciResultante[indiceUltimo - 1];
+    listaFibonacciResultante.push(proximo);
+    ultimo = proximo;
+    indiceUltimo += 1;
+  }  
+
+  return retornarMensagem(n, listaFibonacciResultante);
+}
+
+let n = 1;
 
 while (!isNaN(n)) {
-  n = readline('Digite um número para verificar se está na sequência de Fibonacci: (Qualquer letra para sair) \n');
+  n = readline('Digite um número para verificar se está na sequência de Fibonacci: (Qualquer letra para sair)');
+  const numberToTest = Number(n);
+  console.log('n:', n);
   if (isNaN(n)) {
     console.log('Saindo...');
     return;
   }
-  console.log(fibonacci(n));
+  console.log(fibonacci(numberToTest));
 }
--- fibonacciComPreDefinicaoDeEntradas.js
+++ fibonacciComPreDefinicaoDeEntradas.js
@@ -1,4 +1,23 @@
-// 2) Calcular fibonacci até um índice n
+
+// Função que retorna a mensagem de acordo com a presença do número na sequência de Fibonacci
+const retornarMensagem = (n, listaFibonacciResultante) => {
+  const posicao = listaFibonacciResultante.findIndex((elemento) => elemento === n);
+  
+  const mensagemPositiva = `O numero informado ${n} está na sequência de Fibonacci, com o índice:
+    ${posicao}, ou seja, na ${posicao + 1}ª posição da sequência, em ${listaFibonacciResultante}.\n`;
+  
+  const mensagemNegativa = `O número informado ${n} não está na sequência de Fibonacci,
+    mas a sequência que inclui o maior mais próximo é:
+    ${listaFibonacciResultante}.\n`;
+  
+  if (posicao !== -1) {
+    return mensagemPositiva;
+  }
+  
+  return mensagemNegativa;
+}
+
+// Calcular fibonacci até um índice n
 const fibonacci = (n, primeiro = 0, segundo = 1) => {
   let listaFibonacciResultante = [primeiro, segundo];
   let indiceUltimo = listaFibonacciResultante.length - 1;
@@ -6,13 +25,11 @@ const fibonacci = (n, primeiro = 0, segundo = 1) => {
   let proximo = segundo + primeiro;
   
   if (n === primeiro) {
-    controle += 1;
-    return mensagemPositiva;
+    return retornarMensagem(0, listaFibonacciResultante);
   }
-
+  
   if (n === segundo) {
-    controle += 1;
-    return mensagemPositiva;
+    return retornarMensagem(0, listaFibonacciResultante);
   }
 
   while (ultimo <= n) {    
@@ -20,28 +37,16 @@ const fibonacci = (n, primeiro = 0, segundo = 1) => {
     listaFibonacciResultante.push(proximo);
     ultimo = proximo;
     indiceUltimo += 1;
-  }
+  }  
 
-  const posicao = listaFibonacciResultante.findIndex((elemento) => elemento === n);
-
-  const mensagemPositiva = `O numero informado ${n} está na sequência de Fibonacci, com o índice:
-    ${posicao}, ou seja, na ${posicao + 1}ª posição da sequência, em ${listaFibonacciResultante}.`;
-  
-  const mensagemNegativa = `O número informado ${n} não está na sequência de Fibonacci,
-    mas a sequência que inclui o maior mais próximo é:
-    ${listaFibonacciResultante}.`;
-
-  if (posicao !== -1) {
-    return mensagemPositiva;
-  }
-
-  return mensagemNegativa;
+  return retornarMensagem(n, listaFibonacciResultante);
 }
 
-console.log(fibonacci(2));
+console.log(fibonacci(1));
 console.log(fibonacci(4));
 console.log(fibonacci(6));
 console.log(fibonacci(8));
 console.log(fibonacci(10));
 console.log(fibonacci(15));
 console.log(fibonacci(21));
+console.log(fibonacci(123456789));
